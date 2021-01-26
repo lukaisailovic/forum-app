0db1c4e0df78afe00e708db21c9fe05448705642
Luka Isailovic <lukaisailovic@gmail.com>
2021-01-26 23:01:17 +0100

    User login

--- backend/users/auth.js
+++ backend/users/auth.js
@@ -1,7 +1,9 @@
 const Joi = require('joi');
 const User = require('./model')
 const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 
+const signingSecret = process.env.SIGNING_SECRET || 'secret';
 
 authSchema = Joi.object().keys({
     username: Joi.string().trim().min(4).max(20).required(),
@@ -9,14 +11,55 @@ authSchema = Joi.object().keys({
 
 });
 
-module.exports.login = (req, res) => {
+module.exports.login = async (req, res) => {
+    // validate request
     const { error } = authSchema.validate(req.body)
     if (error) {
         res.status(400).json({
             message: error.details[0].message
         });
     }
+    // get potential user
+    const user = await User.findOne({
+        where: {
+            username: req.body.username
+        },
+    })
+    // check if user exists
+    if(user == null){
+        return res.status(400).json({
+            message: 'User with that username does not exist'
+        });
+    }
+    // check if password match
+    try {
+        if (!await argon2.verify(user.password, req.body.password)) {
+            // password did not match
+            return res.status(400).json({
+                message: 'Password is incorrect'
+            });
+        }
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json({
+            message: 'Could not login user'
+        });
+    }
+    // sign token
+    const token = jwt.sign({
+        data: {
+            username: user.username,
+            id: user.id
+        }
+    }, signingSecret, { expiresIn: '3h'});
+    // return token to user
+    return res.status(200).json({
+        user: {
+            username: user.username
+        },
+        token
 
+    });
 }
 
 module.exports.register = async (req, res) => {
