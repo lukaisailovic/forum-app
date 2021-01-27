70c045485c88eb52b27d7c4b4d20fedd9ec70b54
Luka Isailovic <lukaisailovic@gmail.com>
2021-01-27 12:31:13 +0100

    Remove axios from main app instance

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import axios from 'axios'
+
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
@@ -13,14 +13,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
-
-
-const baseURL = process.env.BACKEND_URL || 'http://localhost:3000/';
-if (typeof baseURL !== 'undefined')
-{
-  axios.defaults.baseURL = baseURL;
-}
-Vue.prototype.$http = axios
 
 
 Vue.config.productionTip = false
